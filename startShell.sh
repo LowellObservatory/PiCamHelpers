efe35713898c99fb6d58102c28c31541b59039d3
Ryan T. Hamilton <astrobokonon@gmail.com>
2020-03-05 12:22:23 -0700

    Remove docker stuff

--- startShell.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#docker run -it -v /opt/vc/lib:/opt/vc/lib --rm -u root --device /dev/vchiq --device /dev/vcsm picamtest /bin/bash
-docker run -it -v /opt/vc/lib:/opt/vc/lib --rm -u lig --device /dev/vchiq --device /dev/vcsm picamtest /bin/bash
